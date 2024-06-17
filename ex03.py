4c122f86dc6062e6158d38b4e103727c2035472a
Ednar Nogueira <edinaredms@gmail.com>
2024-06-17 18:55:35 -0300

    Criando o terceiro exercicio

---
+++ ex03.py
@@ -0,0 +1,8 @@
+"""Escreva um programa que receba dois números do usuário e imprima a soma deles."""
+
+v1 = int(input("informe um valor"))
+v2 = int(input("Informe o segundo valor"))
+
+soma = v1 + v2
+
+print("A soma dos valores informados é de ", soma)
