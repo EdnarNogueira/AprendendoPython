3b900e2f4453fafeed30fd45b0700860667650dd
Ednar Nogueira <edinaredms@gmail.com>
2024-06-17 20:44:34 -0300

    Criando exercicio 05

---
+++ ex05.py
@@ -0,0 +1,9 @@
+"""Peça ao usuario para informar um valor numerico, converta e float e imprima na tela o valor
+e o tipo de dado"""
+
+num = input("Informe um numero")
+
+num = float(num)
+
+print(type(num))
+print(num)
