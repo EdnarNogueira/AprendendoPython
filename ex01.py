6a5854209087ba2e977110c402fdbc5fe42e279e
Ednar Nogueira <edinaredms@gmail.com>
2024-06-17 18:34:43 -0300

    Corrigindo errro do comentario

--- ex01.py
+++ ex01.py
@@ -1,3 +1,3 @@
-nome = str(input("Informe o seu nome"))#criando variável nome e atribuindo a ela o valor informado pelo ussuario
+nome = str(input("Informe o seu nome"))#criando variável nome e atribuindo a ela o valor informado pelo usuario
 
 print("muito prazer em te conhecer {}" .format(nome)) #imprimindo na tela o valor concatenado com a mensagem 
