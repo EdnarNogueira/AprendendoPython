1bce278e9a3c3427781616d2a64a5b5467793bcf
Ednar Nogueira <edinaredms@gmail.com>
2024-06-17 18:46:43 -0300

    Criando segundo exercicio

---
+++ ex02.py
@@ -0,0 +1,5 @@
+"""Crie um programa que peça ao usuario para informar o seu nome e imprima na tela 
+uma mensagem de boas vindas ex. Seja bem vindo _________"""
+
+nome = str(input("Qual é o seu nome? ")) #Entrada de dado recebendo valor do usuario
+print("Muito prazer em te conhecer " ,nome) #imprimindo na tela a mensagem com o nome do usuario
