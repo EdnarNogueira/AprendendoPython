c95fc50efbde57640386cdbf4a2f1beba79797aa
Ednar Nogueira <edinaredms@gmail.com>
2024-06-17 18:32:37 -0300

    inserindo comentarios no exercicio 01

--- ex01.py
+++ ex01.py
@@ -1,3 +1,3 @@
-nome = str(input("Informe o seu nome"))
+nome = str(input("Informe o seu nome"))#criando variável nome e atribuindo a ela o valor informado pelo ussuario
 
-print("muito prazer em te conhecer {}" .format(nome))
+print("muito prazer em te conhecer {}" .format(nome)) #imprimindo na tela o valor concatenado com a mensagem 
